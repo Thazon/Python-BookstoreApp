07a07a55d6f8fb83e6b5879711a85fb1a96f2065
Thazon <badoitiberiualexandru@gmail.com>
2025-08-25 16:01:24 +0300

    Added string casting to string validators, changed error checking from strict to permissive.

--- utils/validators.py
+++ utils/validators.py
@@ -3,40 +3,56 @@ import re
 #Validator class provides functions to check if input is of correct format and length.
 
 #Trims and enforces max length so that the input strings abide by the DB structure
-def validate_string(value: str, max_length: int = None) -> str | None:
+def validate_string(value: str, max_length: int = None) -> None | bool | str:
     if value is None:
         return None
+
+    try:
+        value = str(value)
+    except (TypeError, ValueError):
+        print(f"Invalid string value: {value}")
+        return False
+
     value = value.strip()
+
     if max_length and len(value) > max_length:
         return value[:max_length]
     return value
 
 #Email format validation
-def validate_email(email: str) -> str | None:
-    if email is None:
-        return None
-    email = email.strip()
+def validate_email(email: str) -> None | bool | str:
+    email = validate_string(email)
+
+    if email in (None, False):
+        return email
+
     pattern = r'^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})*$'
     if not re.match(pattern, email):
-        raise ValueError(f"Invalid email format: {email}")
+        print(f"Invalid email format: {email}")
+        return False
     return email
 
 #Convert to int or float then check bounds
-def validate_number(value, num_type=int, minimum=None, maximum=None):
+def validate_number(value: int | float, num_type=int, minimum: int | float = None, maximum: int | float =None) ->(
+        None | bool | int | float):
     if value is None:
         return None
 
     if num_type not in (int, float):
-        raise TypeError(f"Unsupported type {num_type}. Use int or float.")
+        print(f"Unsupported type {num_type}. Use int or float.")
+        return False
 
     try:
         value = num_type(value)
     except (TypeError, ValueError):
-        raise ValueError(f"Invalid {num_type.__name__}: {value}")
+        print(f"Invalid {num_type.__name__}: {value}")
+        return False
 
-    if minimum is not None and value < minimum:
-        raise ValueError(f"Value {value} is below minimum {minimum}.")
-    if maximum is not None and value > maximum:
-        raise ValueError(f"Value {value} is above maximum {maximum}.")
+    if minimum is not None and isinstance(minimum, (int,float)) and value < minimum:
+        print(f"Value {value} is below minimum {minimum}.")
+        return False
+    if maximum is not None and isinstance(maximum, (int, float)) and value > maximum:
+        print(f"Value {value} is above maximum {maximum}.")
+        return False
 
     return value
